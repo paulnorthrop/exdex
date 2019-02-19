a785e6a1305cfcb0288cf50b28f7e0e4a41c8975
Paul Northrop <p.northrop@ucl.ac.uk>
2019-02-19 12:45:27 +0000

    Test for several block sizes [ci skip]

--- tests/testthat/test-sigmahat_dj.R
+++ tests/testthat/test-sigmahat_dj.R
@@ -8,20 +8,19 @@ context("sigmahat_dj")
 # Each row corresponds to a different block
 # Each column corresponds to a differen way to calculate the summation
 # The 1st column is used in estimation of the sampling variance of thetahat
-sigma_mat <- spm_sigmahat_dj(data = newlyn, b = 20, check = TRUE)
 
+# The permitted range of b for these data is 15 - 196
 my_tol <- 1e-5
-
-test_that("Usum1 = Usum", {
-  testthat::expect_equal(sigma_mat[, 2], sigma_mat[, 1], tolerance = my_tol)
-})
-test_that("Usum2 = Usum", {
-  testthat::expect_equal(sigma_mat[, 3], sigma_mat[, 1], tolerance = my_tol)
-})
-test_that("Usum3 = Usum", {
-  testthat::expect_equal(sigma_mat[, 4], sigma_mat[, 1], tolerance = my_tol)
-})
-test_that("Usum4 = Usum", {
-  testthat::expect_equal(sigma_mat[, 5], sigma_mat[, 1], tolerance = my_tol)
-})
-
+b_vec <- c(15, 20, 100, 196)
+for (i in 1:length(b_vec)) {
+  sigma_mat <- spm_sigmahat_dj(data = newlyn, b = b_vec[i], check = TRUE)
+  test_that("Usum2 = Usum", {
+    testthat::expect_equal(sigma_mat[, 2], sigma_mat[, 1], tolerance = my_tol)
+  })
+  test_that("Usum3 = Usum", {
+    testthat::expect_equal(sigma_mat[, 3], sigma_mat[, 1], tolerance = my_tol)
+  })
+  test_that("Usum4 = Usum", {
+    testthat::expect_equal(sigma_mat[, 4], sigma_mat[, 1], tolerance = my_tol)
+  })
+}

9ab21db06028d6ae56fe6d98ecf28dc3cdc95ee9
Paul Northrop <p.northrop@ucl.ac.uk>
2019-03-07 10:13:33 +0000

    Add to maxima tests. [ci skip]

--- tests/testthat/test-maxima.R
+++ tests/testthat/test-maxima.R
@@ -1,16 +1,14 @@
 # Check that the functions disjoint_maxima(), all_disjoint_maxima(),
 # sliding_maxima() and all_maxima() agree and give the correct results
-# on some simple examples
+# on some simple examples and on the newlyn data
 
 my_tol <- 1e-10
-
-# ---------------------------- all_disjoint_maxima() ------------------------ #
 
 context("disjoint_maxima")
 
 x <- 1:9
-temp <- all_disjoint_maxima(x, b = 3)
 index <- c(3, 6, 9)
+temp <- all_disjoint_maxima(x, b = 3)
 y_mat <- matrix(x[index], 3, 1)
 x_mat <- cbind(1:9)
 test_that("x = 1:9, b = 3, all_disjoint_maxima", {
@@ -46,7 +44,50 @@ test_that("x = 1:11, b = 3, all_disjoint_maxima input values", {
   testthat::expect_equal(temp$x_mat[, 1], temp2$x, tol = my_tol)
 })
 
-context("all_maxima()")
+context("sliding_maxima")
+
+# all_maxima() vs truth and all_maxima() vs sliding_maxima()
+
+x <- 1:9
+temp <- all_maxima(x, b = 3)
+index <- 3:9
+y_vec <- x[index]
+x_vec <- 1:9
+test_that("x = 1:9, b = 3, all_maxima", {
+  testthat::expect_equal(temp$ys, y_vec, tol = my_tol)
+})
+test_that("x = 1:9, b = 3, all_maxima input values", {
+  testthat::expect_equal(temp$xs, x_vec, tol = my_tol)
+})
+temp2 <- sliding_maxima(x, b = 3)
+test_that("x = 1:9, b = 3, all_maxima", {
+  testthat::expect_equal(temp2$y, y_vec, tol = my_tol)
+})
+test_that("x = 1:9, b = 3, all_maxima input values", {
+  testthat::expect_equal(temp2$x, x_vec, tol = my_tol)
+})
+
+x <- 1:11
+temp <- all_maxima(x, b = 3)
+index <- c(3, 6, 9, 4, 7, 10, 5, 8, 11)
+y_mat <- matrix(x[index], 3, 3)
+x_mat <- cbind(1:9, 2:10, 3:11)
+test_that("x = 1:11, b = 3, all_maxima", {
+  testthat::expect_equal(temp$yd, y_mat, tol = my_tol)
+})
+test_that("x = 1:11, b = 3, all_maxima input values", {
+  testthat::expect_equal(temp$xd, x_mat, tol = my_tol)
+})
+temp2 <- disjoint_maxima(x, b = 3)
+test_that("x = 1:11, b = 3, all_disjoint_maxima", {
+  testthat::expect_equal(temp$yd[, 1], temp2$y, tol = my_tol)
+})
+test_that("x = 1:11, b = 3, all_disjoint_maxima input values", {
+  testthat::expect_equal(temp$xd[, 1], temp2$x, tol = my_tol)
+})
+
+
+context("all_maxima, newlyn")
 
 # Check that the function all_maxima() gives the same results as the
 # functions all_disjoint_maxima(), disjoint_maxima() and sliding_maxima(),

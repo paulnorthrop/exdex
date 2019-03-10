24f951b86b669ccebc20011ccc6c8efc354f9210
Paul Northrop <p.northrop@ucl.ac.uk>
2019-03-10 01:23:17 +0000

    Try again

--- tests/testthat/test-confint.R
+++ tests/testthat/test-confint.R
@@ -1,8 +1,8 @@
 context("confint.exdex")
 
 res <- spm(newlyn, 100)
-ci1 <- confint(res, plot = TRUE, type = "cholesky")
-ci2 <- confint(res, plot = TRUE, type = "spectral")
+ci1 <- confint(res, type = "cholesky")
+ci2 <- confint(res, type = "spectral")
 
 test_that("cholesky and spectral are identical", {
   testthat::expect_identical(ci1, ci2)

518612bb4e88c946da9b157abf55d77989679404
Paul Northrop <p.northrop@ucl.ac.uk>
2019-03-10 23:10:27 +0000

    Delete comments

--- R/confidence_intervals.R
+++ R/confidence_intervals.R
@@ -145,11 +145,9 @@ confint.spm <- function (object, parm = "theta", level = 0.95,
   uncon <- coef(object, maxima = maxima, estimator = "both", constrain = TRUE)
   se <- sqrt(vcov(object, maxima = maxima, estimator = "both"))
   if (maxima == "sliding") {
-#    uncon <- object$uncon_theta_sl
     yz_data <- object$data_sl
     bias_val <- object$bias_sl
   } else {
-#    uncon <- object$uncon_theta_dj
     yz_data <- object$data_dj
     bias_val <- object$bias_dj
   }

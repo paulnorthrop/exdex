2c45aaec57530ead6d5faa12dce7447489c24731
Paul Northrop <p.northrop@ucl.ac.uk>
2019-03-10 01:34:33 +0000

    Add test

--- tests/testthat/test-confint.R
+++ tests/testthat/test-confint.R
@@ -1,10 +1,29 @@
 context("confint.exdex")
 
 res <- spm(newlyn, 100)
+
 ci1 <- confint(res, type = "cholesky")
 ci2 <- confint(res, type = "spectral")
-
-test_that("cholesky and spectral are identical", {
+test_that("sliding: cholesky and spectral are identical", {
   testthat::expect_identical(ci1, ci2)
 })
 
+ci1 <- confint(res, parm = "disjoint", type = "cholesky")
+ci2 <- confint(res, parm = "disjoint", type = "spectral")
+test_that("disjoint: cholesky and spectral are identical", {
+  testthat::expect_identical(ci1, ci2)
+})
+
+
+res <- suppressWarnings(spm(newlyn, 14))
+ci_b_low <- confint(res)
+temp <- matrix(NA, nrow = 4, ncol = 2)
+level <- 0.95
+a <- (1 - level) / 2
+a <- c(a, 1 - a)
+pct <- paste(round(100 * a, 1), "%")
+colnames(temp) <- pct
+rownames(temp) <- c("N2015sym", "BB2018sym", "N2015lik", "BB0218lik")
+test_that("b is too low gives NAs", {
+  testthat::expect_identical(ci_b_low, temp)
+})

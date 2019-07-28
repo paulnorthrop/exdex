79827d8ab4f7945b184c9aef728daebe23e516c9
Paul Northrop <p.northrop@ucl.ac.uk>
2019-07-28 09:02:26 +0100

    IMT is now imt

--- tests/testthat/test-kgaps_imt.R
+++ tests/testthat/test-kgaps_imt.R
@@ -6,7 +6,7 @@ context("kgaps_imt")
 thresh <- stats::quantile(newlyn, probs = c(0.85, 0.90, 0.95))
 k_vals <- 1:4
 all_res <- kgaps_imt(newlyn, thresh, k_vals)
-all_IMT <- all_res$IMT
+all_IMT <- all_res$imt
 all_p <- all_res$p
 all_theta <- all_res$theta
 
@@ -14,7 +14,7 @@ ind_IMT <- ind_p <- ind_theta <- all_IMT
 for (i in 1:length(thresh)) {
   for (j in 1:length(k_vals)) {
     temp <- kgaps_imt(newlyn, thresh = thresh[i], k = k_vals[j])
-    ind_IMT[i, j] <- temp$IMT
+    ind_IMT[i, j] <- temp$imt
     ind_p[i, j] <- temp$p
     ind_theta[i, j] <- temp$theta
   }

d1faa25e8a701ac9f37cb33eac28b48cd762f939
Paul Northrop <p.northrop@ucl.ac.uk>
2022-03-24 15:26:17 +0000

    Add inc_cens = TRUE tests

--- tests/testthat/test-kgaps.R
+++ tests/testthat/test-kgaps.R
@@ -2,9 +2,11 @@
 
 # Check that summary.kgaps() returns the correct values
 
+# For inc_cens = FALSE -----
+
 ### S&P 500 index
 u <- quantile(sp500, probs = 0.60)
-theta <- kgaps(sp500, u)
+theta <- kgaps(sp500, u, inc_cens = FALSE)
 res1 <- signif(c(theta$theta, theta$se),
                digits = max(3, getOption("digits") - 3L))
 res2 <- summary(theta)$matrix
@@ -15,7 +17,7 @@ test_that("Fitted object and summary() agree", {
 
 # Check that kgaps() gives the same output for vector and matrix data input
 
-theta2 <- kgaps(as.matrix(sp500), u)
+theta2 <- kgaps(as.matrix(sp500), u, inc_cens = FALSE)
 
 theta$call <- NULL
 theta2$call <- NULL
--- tests/testthat/test-kgaps_imt.R
+++ tests/testthat/test-kgaps_imt.R
@@ -3,9 +3,12 @@
 # Check that calling kgaps_imt() with vector arguments u and k gives
 # the same results as calling kgaps_imt() repeatedly with scalar arguments
 
+# For inc_cens = FALSE -----
+inc_cens <- FALSE
+
 u <- stats::quantile(newlyn, probs = c(0.85, 0.90, 0.95))
 k_vals <- 1:4
-all_res <- kgaps_imt(newlyn, u, k_vals)
+all_res <- kgaps_imt(newlyn, u, k_vals, inc_cens = inc_cens)
 all_IMT <- all_res$imt
 all_p <- all_res$p
 all_theta <- all_res$theta
@@ -13,7 +16,39 @@ all_theta <- all_res$theta
 ind_IMT <- ind_p <- ind_theta <- all_IMT
 for (i in 1:length(u)) {
   for (j in 1:length(k_vals)) {
-    temp <- kgaps_imt(newlyn, u = u[i], k = k_vals[j])
+    temp <- kgaps_imt(newlyn, u = u[i], k = k_vals[j], inc_cens = inc_cens)
+    ind_IMT[i, j] <- temp$imt
+    ind_p[i, j] <- temp$p
+    ind_theta[i, j] <- temp$theta
+  }
+}
+
+my_tol <- 1e-5
+
+test_that("IMT values agree", {
+  testthat::expect_equal(all_IMT, ind_IMT, tolerance = my_tol)
+})
+test_that("p-values agree", {
+  testthat::expect_equal(all_p, ind_p, tolerance = my_tol)
+})
+test_that("MLEs of theta values agree", {
+  testthat::expect_equal(all_theta, ind_theta, tolerance = my_tol)
+})
+
+# For inc_cens = TRUE -----
+inc_cens <- TRUE
+
+u <- stats::quantile(newlyn, probs = c(0.85, 0.90, 0.95))
+k_vals <- 1:4
+all_res <- kgaps_imt(newlyn, u, k_vals, inc_cens = inc_cens)
+all_IMT <- all_res$imt
+all_p <- all_res$p
+all_theta <- all_res$theta
+
+ind_IMT <- ind_p <- ind_theta <- all_IMT
+for (i in 1:length(u)) {
+  for (j in 1:length(k_vals)) {
+    temp <- kgaps_imt(newlyn, u = u[i], k = k_vals[j], inc_cens = inc_cens)
     ind_IMT[i, j] <- temp$imt
     ind_p[i, j] <- temp$p
     ind_theta[i, j] <- temp$theta

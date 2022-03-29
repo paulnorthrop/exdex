c16ffd6a584007643598598611a8a7c133f34637
Paul Northrop <p.northrop@ucl.ac.uk>
2022-03-29 23:41:03 +0100

    Add tests

--- tests/testthat/test-dgaps.R
+++ tests/testthat/test-dgaps.R
@@ -1,6 +1,6 @@
 #context("summary.dgaps")
 
-# Check that summary.dgaps() returns the correct values
+### Check that summary.dgaps() returns the correct values
 
 # For inc_cens = FALSE -----
 
@@ -17,6 +17,8 @@ test_that("Fitted object and summary() agree", {
 
 # Check that dgaps() gives the same output for vector and matrix data input
 
+# For inc_cens = FALSE -----
+
 theta2 <- dgaps(as.matrix(sp500), u, inc_cens = FALSE)
 
 theta$call <- NULL
@@ -25,3 +27,32 @@ theta2$call <- NULL
 test_that("dgaps: vector data vs matrix data", {
   testthat::expect_equivalent(theta, theta2)
 })
+
+### Check that summary.dgaps() returns the correct values
+
+# For inc_cens = TRUE -----
+
+### S&P 500 index
+u <- quantile(sp500, probs = 0.60)
+theta <- dgaps(sp500, u, inc_cens = TRUE)
+res1 <- signif(c(theta$theta, theta$se),
+               digits = max(3, getOption("digits") - 3L))
+res2 <- summary(theta)$matrix
+
+test_that("Fitted object and summary() agree", {
+  testthat::expect_equivalent(res1, res2)
+})
+
+# Check that dgaps() gives the same output for vector and matrix data input
+
+# For inc_cens = TRUE -----
+
+theta2 <- dgaps(as.matrix(sp500), u, inc_cens = TRUE)
+
+theta$call <- NULL
+theta2$call <- NULL
+
+test_that("dgaps: vector data vs matrix data", {
+  testthat::expect_equivalent(theta, theta2)
+})
+

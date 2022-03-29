6ba5bed647670a8ca7d959e74f50e2e23dddb4bf
Paul Northrop <p.northrop@ucl.ac.uk>
2022-03-29 23:28:28 +0100

    Add tests

---
+++ tests/testthat/test-dgaps.R
@@ -0,0 +1,27 @@
+#context("summary.dgaps")
+
+# Check that summary.dgaps() returns the correct values
+
+# For inc_cens = FALSE -----
+
+### S&P 500 index
+u <- quantile(sp500, probs = 0.60)
+theta <- dgaps(sp500, u, inc_cens = FALSE)
+res1 <- signif(c(theta$theta, theta$se),
+               digits = max(3, getOption("digits") - 3L))
+res2 <- summary(theta)$matrix
+
+test_that("Fitted object and summary() agree", {
+  testthat::expect_equivalent(res1, res2)
+})
+
+# Check that dgaps() gives the same output for vector and matrix data input
+
+theta2 <- dgaps(as.matrix(sp500), u, inc_cens = FALSE)
+
+theta$call <- NULL
+theta2$call <- NULL
+
+test_that("dgaps: vector data vs matrix data", {
+  testthat::expect_equivalent(theta, theta2)
+})

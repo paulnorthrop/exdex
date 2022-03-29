7d730c4a02ab416b877fa48022f9837d8cdbea2c
Paul Northrop <p.northrop@ucl.ac.uk>
2022-03-29 21:21:40 +0100

    Explain that K-gaps right-censored at 0 add no information

--- R/kgaps.R
+++ R/kgaps.R
@@ -178,7 +178,8 @@ kgaps <- function(data, u, k = 1, inc_cens = TRUE) {
 #'    \eqn{S_0} is greater than zero and \eqn{I_0 = 0} otherwise, and similarly
 #'    for \eqn{I_N}.
 #'    The differing treatment of uncensored and right-censored \eqn{K}-gaps
-#'    reflects differing contributions to the likelihood.
+#'    reflects differing contributions to the likelihood.  Right-censored
+#'    \eqn{K}-gaps that are equal to zero add no information to the likelihood.
 #'    For full details see Suveges and Davison (2010) and Attalides (2015).
 #'
 #'    If \eqn{N_1 = 0} then we are in the degenerate case where there is one
@@ -235,22 +236,22 @@ kgaps_stat <- function(data, u, k = 1, inc_cens = TRUE) {
   sum_qs <- sum(q_u * S_k)
   # Store the number of K-gaps, for use by nobs.kgaps()
   n_kgaps <- N0 + N1
-  # Include censored inter-exceedance times?
+  # Include right-censored inter-exceedance times?
   if (inc_cens) {
-    # censored inter-exceedance times and K-gaps
+    # Right-censored inter-exceedance times and K-gaps
     T_u_cens <- c(exc_u[1] - 1, nx - exc_u[N_u])
     S_k_cens <- pmax(T_u_cens - k, 0)
     # N0, N1, sum of scaled K-gaps
     # S_k_cens = 0 adds no information, because P(S >= 0) = 1
     N1_cens <- sum(S_k_cens > 0)
     n_kgaps <- n_kgaps + N1_cens
-    # Remove the censored K-gaps that are equal to zero
+    # Remove the right-censored K-gaps that are equal to zero
     # (This is is not necessary here, but we do it for clarity)
     S_k_cens <- S_k_cens[S_k_cens > 0]
     sum_s_cens <- sum(q_u * S_k_cens)
     # Add contributions.
-    # Note: we divide N1_cens by two because a censored non-zero K-gap S_c
-    # contributes theta exp(-theta q_u S_c) to the K-gaps likelihood,
+    # Note: we divide N1_cens by two because a right-censored non-zero K-gap
+    # S_c contributes theta exp(-theta q_u S_c) to the K-gaps likelihood,
     # whereas a non-censored non-zero K-gap contributes
     # theta^2 exp(-theta q_u S_c).
     # See equation (4.3) of Attalides (2015)

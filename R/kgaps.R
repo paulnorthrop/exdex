154bfcb13f9310cead284a8de217a1d029a3a849
Paul Northrop <p.northrop@ucl.ac.uk>
2023-02-10 18:01:36 +0000

    If K = 0 then return se = NA

--- R/kgaps.R
+++ R/kgaps.R
@@ -54,7 +54,8 @@
 #'     \item{\code{theta} }{The maximum likelihood estimate (MLE) of
 #'       \eqn{\theta}.}
 #'     \item{\code{se} }{The estimated standard error of the MLE, calculated
-#'       using an algebraic expression for the observed information.}
+#'       using an algebraic expression for the observed information.
+#'       If \code{k = 0} then \code{se} is \code{NA}.}
 #'     \item{\code{se_exp} }{The estimated standard error of the MLE,
 #'       calculated using an algebraic expression for the expected information.
 #'       If the estimate of \eqn{\theta} is 0 or 1 then \code{se_exp} is
@@ -159,6 +160,11 @@ kgaps <- function(data, u, k = 1, inc_cens = TRUE) {
     obs_info <- obs_info + 2 * N1 / theta_mle ^ 2
   }
   theta_se <- sqrt(1 / obs_info)
+  # If K = 0 then the estimate of theta is 1 by default and returning a SE
+  # does not seem appropriate
+  if (k == 0) {
+    theta_se <- NA
+  }
   max_loglik <- do.call(kgaps_loglik, c(list(theta = theta_mle), ss))
   res <- list(theta = theta_mle, se = theta_se, se_exp = se_exp, ss = ss,
               k = k, u = u, inc_cens = inc_cens, max_loglik = max_loglik,
